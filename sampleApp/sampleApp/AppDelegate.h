c61bd9a31af9ba160ff9c7db6599ef89a22a04fb
DheerajSR <bagoria2011@gmail.com>
2013-08-12 12:23:41 +0530

    Sample App Commit

---
+++ sampleApp/sampleApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  sampleApp
+//
+//  Created by The Geeks on 8/12/13.
+//  Copyright (c) 2013 WsCube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ sampleApp/sampleApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sampleApp
+//
+//  Created by The Geeks on 8/12/13.
+//  Copyright (c) 2013 WsCube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
